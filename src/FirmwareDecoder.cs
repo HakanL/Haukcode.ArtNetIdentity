cb65f4e961e43e9402a4af952b49efeaec81bae3
Hakan Lindestaf <hakan@lindestaf.com>
2026-02-04 23:01:14 -0600

    Initial code commit

---
+++ src/FirmwareDecoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Haukcode.ArtNetIdentity
+{
+    public static class FirmwareDecoder
+    {
+        /// <summary>
+        /// Decodes the firmware version for known manufacturers.
+        /// </summary>
+        /// <param name="estaCode">Manufacturer ID</param>
+        /// <param name="oem">OEM code (not relevant for all manufacturers as they may use the same firmware version schema for all their models</param>
+        /// <param name="firmwareVersion">The firmware version field in the ArtPollReply packet</param>
+        /// <returns>String representation of the firmware version, or null if unsupported/unknown</returns>
+        public static string? GetFirmwareVersion(int estaCode, int oem, short firmwareVersion)
+        {
+            switch (estaCode)
+            {
+                case 0x6A6B:        // JPK Systems Limited
+                    return $"{firmwareVersion >> 8}.{firmwareVersion & 0xff}";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/ManufacturerModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Haukcode.ArtNetIdentity
+{
+    public static class ManufacturerModel
+    {
+        /// <summary>
+        /// Returns the model name for the given ESTA and OEM code.
+        /// </summary>
+        /// <param name="estaCode">Manufacturer ID</param>
+        /// <param name="oem">OEM code</param>
+        /// <returns>Name of the device model, or null if one isn't known</returns>
+        public static string? GetModelName(int estaCode, int oem)
+        {
+            switch (estaCode)
+            {
+                case 0x6A6B:        // JPK Systems Limited
+                    switch (oem)
+                    {
+                        case 0x2BD2:
+                            return "LeDMX4 MAX";
+
+                        case 0x2BD3:
+                            return "LeDMX2 MAX";
+                    }
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
